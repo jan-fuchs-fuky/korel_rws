376764dcf67e6e7b4d02b3fbba0aae831b698e54
Jan Fuchs <fuky@asu.cas.cz>
2009-11-05 23:04:44 +0000

    rm *.f

--- bin/korelpar.f
+++
@@ -1,8 +0,0 @@
-      parameter (nsp=300,npx=1024,npx2=2048,mnu=5,mnsu=15,mtmp=3)
-c nsp = maximum number of input spectra
-c npx = maximum number of pixels in each spectrum
-c npx2 = 2*npx
-c mnu = maximum number of spectral regions (>nu=number of sp. regions)
-c mnsu = maximum number of component spectra (>nu*ns, ns=num. of comp.)
-
-
