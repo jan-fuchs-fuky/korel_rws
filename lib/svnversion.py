dfb064599803ef06bbee585e0fd4e6189b9ac0a5
Jan Fuchs <fuky@asu.cas.cz>
2011-07-19 08:48:16 +0000

    lib/svnversion.py: aktualizace

--- lib/svnversion.py
+++ lib/svnversion.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python2.6
+#!/usr/bin/env python
 
 # Generated automatically from ../make.svnversion. DO NOT EDIT.
 
 def get():
-    return "119"
+    return "133"
 
 def date():
-    return "Last Changed Date: 2011-07-15 12:27:07 +0200 (Fri, 15 Jul 2011)"
+    return "Last Changed Date: 2011-07-19 10:46:40 +0200 (Tue, 19 Jul 2011)"
