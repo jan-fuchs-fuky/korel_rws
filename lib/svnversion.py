64051cabaf0afc06173068a5564d51a80bf72dfe
Jan Fuchs <fuky@asu.cas.cz>
2019-01-15 12:47:18 +0100

    lib/svnversion.py make.svnversion
        - vyuziva:
    
            git log --pretty=format:"%h" -1
            git log --pretty=format:"%ad" -1

--- lib/svnversion.py
+++ lib/svnversion.py
@@ -3,7 +3,7 @@
 # Generated automatically from ../make.svnversion. DO NOT EDIT.
 
 def get():
-    return "133"
+    return "b13e61a"
 
 def date():
-    return "Last Changed Date: 2011-07-19 10:46:40 +0200 (Tue, 19 Jul 2011)"
+    return "Tue Jan 15 12:38:02 2019 +0100"
